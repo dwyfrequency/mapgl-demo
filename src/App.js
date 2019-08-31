8bfb71f857a6655194a41953185edaa058164f18
Jack Dwyer <jdwyer@Jacks-MacBook-Pro-2.local>
2019-08-31 11:03:47 -0400

    adding initial map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+console.log(process.env)
+
+// ES6
+
+const Map = ReactMapboxGl({
+  accessToken:
+  process.env.REACT_APP_MAPGL_ACCESS_CODE
+});
+
+
+
 
 function App() {
   return (
@@ -19,6 +31,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: '100vh',
+          width: '100vw'
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
     </div>
   );
 }
