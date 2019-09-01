b50ad004306c2271af41e5fe11b94608db131fa7
Jack Dwyer <jdwy215@gmail.com>
2019-09-01 12:28:53 -0400

    creating seperate folder for recom and list components

---
+++ src/components/VenueRecs/VenueItems.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import VenueCard from "./VenueCard";
+
+const VenueItems = ({ venues }) => {
+  return <h1></h1>;
+  // return venues.map(venue => <VenueCard {...venue} key={venue.id} />);
+};
+
+export default VenueItems;
--- src/components/Venues/VenuesList.js
+++ src/components/Venues/VenuesList.js
@@ -1,33 +1,30 @@
 import React, { useState, useEffect } from "react";
-import SearchBar from "./SearchBar";
+import SearchBar from "../SearchBar";
 import VenueItems from "./VenueItems";
 
-const VenuesListRecom = ({ foursquare }) => {
+const VenuesList = ({ foursquare }) => {
   const [query, setQueryVal] = useState("");
   const [venues, setVenues] = useState([]);
 
   useEffect(() => {
     const getVenues = async (query, ll) => {
-      const data = await foursquare.venues.recommendations({
+      const data = await foursquare.venues.getVenues({
         query,
         ll: ll.join(","),
         limit: 5
       });
-      console.log({ data });
-      setVenues(data.response.group.results || []);
+      setVenues(data.response.venues);
     };
     if (query) getVenues(query, [40.73061, -73.935242]);
   }, [query, foursquare]);
 
-  console.log({ venues });
   return (
     <div className="App">
       <SearchBar setSearchVal={setQueryVal} />
-      {venues.length ? JSON.stringify(venues) : null}
-      {/* {venues.length ? <VenueItems venues={venues} /> : null} */}
+      {venues.length ? <VenueItems venues={venues} /> : null}
       {/* <MapGL></MapGL> */}
     </div>
   );
 };
 
-export default VenuesListRecom;
+export default VenuesList;
